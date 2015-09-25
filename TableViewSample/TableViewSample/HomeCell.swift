4dfc0947e3fe90658304a960260b11d839c75886
Thang Truong <tcthang.dsna@gmail.com>
2015-09-25 10:32:15 +0700

    first commit

---
+++ TableViewSample/TableViewSample/HomeCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeCell.swift
+//  TableViewSample
+//
+//  Created by Thang Truong on 9/25/15.
+//  Copyright (c) 2015 Thang Truong. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+    
+    @IBOutlet weak var labelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
+
---
+++ TableViewSample/TableViewSample/HomeController.swift
@@ -0,0 +1,47 @@
+//
+//  HomeController.swift
+//  TableViewSample
+//
+//  Created by Thang Truong on 9/25/15.
+//  Copyright (c) 2015 Thang Truong. All rights reserved.
+//
+
+import UIKit
+
+class HomeController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let CELL_IDENTIFER: String = "home_cell"
+    private let LANGUAGES: [String] = ["Java", "Swift", "Object-C", "C++"]
+    
+    @IBOutlet weak var tableViewHome: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableViewHome.registerNib(UINib(nibName: "HomeCell", bundle: nil), forCellReuseIdentifier: CELL_IDENTIFER)
+        self.tableViewHome.dataSource = self
+        self.tableViewHome.delegate = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return LANGUAGES.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var homeCell: HomeCell? = self.tableViewHome.dequeueReusableCellWithIdentifier(CELL_IDENTIFER) as! HomeCell?
+        homeCell?.labelName.text = LANGUAGES[indexPath.row]
+        
+        return homeCell!
+    }
+    
+}
